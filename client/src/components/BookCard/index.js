68e28c4c2fde5837ce67af907c3a04d8e4408f7d
Greg Baker <semperfifcd@gmail.com>
2019-07-15 19:16:26 -0400

    Components, nearly finished.

---
+++ client/src/components/BookCard/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function BookCard() {
+    return (
+        <div>
+            <h3>
+                This is a Book Card!
+            </h3>
+        </div>
+    );
+}
+
+export default BookCard;
+
+
--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -3,9 +3,30 @@ import "./style.css";
 
 function Search() {
     return (
-        <h1>Search component content</h1>
+        <div>
+            <form>
+                <div class="form-group">
+                    <h3>Book Search</h3>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter a book" />
+                </div>
+                <button type="submit" class="btn btn-primary">Search</button>
+            </form>
+
+            <div class="resultsDiv">
+
+            </div>
+        </div>
+
     );
-}
+};
+
+function doSearch() {
+
+};
+
+function displayResults() {
+
+};
 
 export default Search;
 
