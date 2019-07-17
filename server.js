8c7175fc77e910eeb2607c9ab6db46d1f6865fa0
Greg Baker <semperfifcd@gmail.com>
2019-07-17 19:53:13 -0400

    Changed mongoose connection to or statement between local and mongooselab.

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect("mongodb://localhost/googlebooks", { useNewUrlParser: true } || process.env.MONGODB_URI);
 
 // Define API routes here
 
